ed71f2a13176418dba3c31fb29563139fb5b799a
devNikhil <168222942+Web-Dev-Hero@users.noreply.github.com>
2024-05-05 15:18:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Data from "./Data";
+
+function App(){
+return(
+  <div className="flex justify-center items-center h-screen">
+    <h1 className=  " demo bg-red-400 w-fit m-5 p-4 text-lg font-bold rounded-md hover:bg-red-600 cursor-pointer ">
+      i am tailwind</h1>
+    <Data></Data>
+  </div>
+)
+
+
+
+}
+export default App;
---
+++ src/Data.jsx
@@ -0,0 +1,15 @@
+
+
+import { defaults } from "autoprefixer";
+import React from "react";
+
+function Data (){
+return(
+  <>
+  <h1 className="text-xl text-black-400 border-solid-2px justify-normal font-mono bg-green-400 font-bold w-fit p-3 mx-3  ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde commodi tempore repellat illo ex doloribus repudiandae eum dignissimos excepturi debitis.
+  </h1>
+  
+ </>
+)
+}
+export default Data;
